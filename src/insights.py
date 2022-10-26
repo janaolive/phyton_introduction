0fcc61a997e8d2cb5693e8d671a7ccb7d00f2f00
Janaina Oliveira <janainapp@gmail.com>
2022-10-26 19:36:18 -0300

    fix: req6 e 7

--- src/insights.py
+++ src/insights.py
@@ -12,21 +12,8 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    filter_job_type = [job for job in jobs if job["job_type"] == job_type]
+    return filter_job_type
 
 
 def get_unique_industries(path):
@@ -40,21 +27,8 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filter_industry = [job for job in jobs if job["industry"] == industry]
+    return filter_industry
 
 
 def get_max_salary(path):
