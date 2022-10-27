6d9ec9d4bb9df57cf256f05a2547a391e6787d34
Janaina Oliveira <janainapp@gmail.com>
2022-10-27 19:10:07 -0300

    feat: code cleanup req8 e 9

--- src/insights.py
+++ src/insights.py
@@ -67,28 +67,6 @@ def matches_salary_range(job, salary):
     else:
         return lower_salary <= salary <= higher_salary
 
-    """Checks if a given salary is in the salary range of a given job
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
-
-
 def filter_by_salary_range(jobs, salary):
     salary_list = list()
     for job in jobs:
@@ -98,15 +76,3 @@ def filter_by_salary_range(jobs, salary):
         except ValueError:
             pass
     return salary_list
-    """Filters a list of jobs by salary range
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
