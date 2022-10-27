4c84f4d2a0b9b089930afbde5521762604c47343
Janaina Oliveira <janainapp@gmail.com>
2022-10-27 18:56:42 -0300

    fix: req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    "Check the occurrence count of the word"
+    assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "Javascript") == 122
